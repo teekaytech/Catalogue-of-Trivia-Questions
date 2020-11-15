9d9acd713bbe456bf7a030643c170980c912156e
teekaytech <taofeek.olalere@feghas.com>
2020-11-15 16:06:32 +0100

    Exclude redux store in production mode

--- src/store.js
+++ src/store.js
@@ -12,7 +12,9 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    process.env.NODE_ENV !== 'production'
+      && window.__REDUX_DEVTOOLS_EXTENSION__
+      && window.__REDUX_DEVTOOLS_EXTENSION__(),
   ),
 );
 
